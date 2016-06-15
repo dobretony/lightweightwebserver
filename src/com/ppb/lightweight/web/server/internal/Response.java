abf67d759365c93e7d4bb55c7b4c4144090a0122
Dobre Antonel <antonel.g.dobre@intel.com>
2016-06-15 19:16:50 +0300

    Adding Request and Response
    Adding Request and Response classes that will handle
    the HTTPRequest and HTTPResponse header messages.
    
    Signed-off-by: Dobre Antonel <antonel.g.dobre@intel.com>

---
+++ src/com/ppb/lightweight/web/server/internal/Response.java
@@ -0,0 +1,18 @@
+package com.ppb.lightweight.web.server.internal;
+
+/**
+ * Created by tony on 15.06.2016.
+ */
+public class Response {
+
+    public enum RESPONSE_TYPE {
+
+    };
+
+    private String responseHeader = null;
+
+    public Response(){
+
+    }
+
+}
